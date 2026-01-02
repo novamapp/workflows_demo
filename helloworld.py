2e029023f839e0e7a97d2e287845bddca88af637
novamapp <55009153+novamapp@users.noreply.github.com>
2026-01-01 21:38:12 -0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,10 @@
+import sys
+
 def main():
-    print('Hello world!')
+    print(f'Hello world! From Python: {sys.version_info}')
+
+    if sys.version_info >= (3, 7) and sys.version_info < (3, 8):
+        raise Exception('Python version 3.7.x is unsupported!')
 
 if __name__ == "__main__":
     main()
